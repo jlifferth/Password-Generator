8553c7ec4506528bf979e393fc6ad8a40acc6fb7
Jonathan Lifferth <jonathan.lifferth@gmail.com>
2021-12-28 19:36:31 -0700

    added feature to toggle special characters in password

--- main.py
+++ main.py
@@ -2,13 +2,16 @@ import streamlit as st
 import random
 
 
-def generate_password(length):
-    chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890'
-    print(len(chars))
+def generate_password(length, special):
+    if special:
+        chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()'
+    else:
+        chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890'
+    # print(len(chars))
     password = ''
     for i in range(length):
         index = random.randint(0, (len(chars) - 1))
-        print(index)
+        # print(index)
         char = chars[index]
         password += char
     return password
@@ -22,7 +25,13 @@ st.title('Random Password Generator')
 number = st.number_input('Password length', min_value=1)
 st.write('Password length : ' + str(number))
 
+special_chars = True
+if st.checkbox('Include Special characters'):
+    special_chars = True
+else:
+    special_chars = False
+
 if st.button('Generate Password'):
-    st.text(generate_password(int(number)))
+    st.text(generate_password(int(number), special=special_chars))
 else:
     st.text('Password will display here')
